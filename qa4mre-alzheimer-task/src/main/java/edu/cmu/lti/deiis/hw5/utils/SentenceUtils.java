d62398138e09d5634093b81d35bf975a824ee043
kohlisimranjit <kohlisimranjit@gmail.com>
2013-12-04 22:27:04 -0500

    added new uilities calss for filter

---
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/utils/SentenceUtils.java
@@ -0,0 +1,123 @@
+package edu.cmu.lti.deiis.hw5.utils;
+
+import java.util.List;
+
+import org.apache.uima.jcas.cas.FSList;
+
+import edu.cmu.lti.qalab.types.Answer;
+import edu.cmu.lti.qalab.types.CandidateAnswer;
+import edu.cmu.lti.qalab.types.NounPhrase;
+import edu.cmu.lti.qalab.types.Sentence;
+import edu.cmu.lti.qalab.utils.Utils;
+
+public class SentenceUtils
+
+{
+	
+	static boolean checkNPList(
+			List<NounPhrase> list, String cat)
+	{
+		for(NounPhrase np:list)
+		{
+		String nptext=np.getText()	;
+
+		if(WordNetAPI.getWordHypernyms(nptext, null, cat, null))
+			return true;
+
+		else{String terms[]=nptext.split(" ");
+
+		for(String term :terms)
+		{
+			if(WordNetAPI.getWordHypernyms(term, null, cat, null))
+				return true;	
+		}
+
+		}
+
+		}
+		return false;}
+
+	
+	
+	static boolean checkNPList(
+			String text, String cat)
+	{
+		String[] terms=text.split(" ");
+		
+		for(String np:terms)
+		{
+		String nptext=np;
+
+		if(WordNetAPI.getWordHypernyms(nptext, null, cat, null))
+			return true;
+
+		}
+		
+		
+		return false;}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	static boolean doesAnsweMatchCategory(Answer ans, String cat)
+{
+	
+FSList fslist=ans.getNounPhraseList();
+
+
+List<NounPhrase> list=Utils.fromFSListToCollection(fslist, NounPhrase.class);
+
+return checkNPList(list,cat);
+}
+
+
+
+
+
+static boolean doesCandAnswerMatchCategory(CandidateAnswer cand,String cat)
+{String text=cand.getText();
+
+if(checkNPList(text,cat))
+	return true;
+
+String coveredText=cand.getCoveredText();
+if(checkNPList(coveredText,cat))
+	return true;
+
+return false;
+}
+
+static boolean doesSentenceMatchCategory(Sentence sent, String cat)
+{
+	FSList fslist=	sent.getPhraseList();
+
+	List<NounPhrase> list=Utils.fromFSListToCollection(fslist, NounPhrase.class);
+	return checkNPList(list,cat);
+
+}
+
+
+
+
+
+
+
+
+}
